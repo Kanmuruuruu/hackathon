f89522c6eac1643328f4b5b7e3c5fd1b712b6da5
Gautier Gueri <gautiergueri@gmail.com>
2019-12-19 17:24:38 +0100

    Itinéraire plus css qui marche, sans valeur de départ mais avec GPS ca marche

--- src/components/Search.js
+++ src/components/Search.js
@@ -27,8 +27,8 @@ function formatDuration (reste) {
         year:0,
         day:0,
         hour:0,
-        minute:0,
-        seconde:0,
+        min:0,
+        sec:0,
     }
     while(reste>=31536000){
         readable.year++;
@@ -42,8 +42,8 @@ function formatDuration (reste) {
         readable.hour++;
         reste-=3600;
     }
-    readable.minute=Math.floor(reste/60);
-    readable.seconde=reste%60;
+    readable.min=Math.floor(reste/60);
+    readable.sec=reste%60;
     const aux = returnString(readable);
     //Make sure to add "," and "and" whe it's necessary.
     let res="";
@@ -64,11 +64,12 @@ function formatDuration (reste) {
 const Search = () => {
 
     const [coordonnees,setCoordonnees] = useState({
-        lat: "1.90392",
-        long:"47.90243"
+        lat: "", // 1.90392
+        long:"" // 47.90243
     });
-    const [itineraire, setItineraire] = useState([]);
+
     const [perturbation, setPerturbation] =useState([]);
+    const [journeys, setJourneys] = useState([]);
 
 
     const getDuree = duree => {
@@ -77,7 +78,7 @@ const Search = () => {
 
     const getHeure = h => {
         const heure = h.split('T')[1];
-        return (<p>{heure[0]+heure[1]} H {heure[2]+heure[3]} M</p>)
+        return (<p>: {heure[0]+heure[1]}:{heure[2]+heure[3]}</p>)
     }
 
     /*
@@ -90,41 +91,49 @@ const Search = () => {
     */
 
     const getItineraire = async () => {
-        console.log(coordonnees);
         const lat=coordonnees.lat;
         const long=coordonnees.long;
         const response = await fetch("https://api.navitia.io/v1/coverage/fr-cen/journeys?from=1.89473%3B47.89386&to="+lat+"%3B"+long+"&",{headers: {Authorization: 'a3653e1d-06a1-4edc-b768-c9bd561d3251'}});
         const data = await response.json();
         setPerturbation(data.disruptions);
-        setItineraire(data.journeys[0].sections);
+        setJourneys(data.journeys);
     }
 
     const renduIt = (index,it) => {
-        console.log(it);
         switch (it.type) {
             case "street_network":
                 return (
                     <div key={index} className="marche">
-                        <p>Marche</p>
+                        <h1>Marche</h1>
+                        <p>{it.to.name}</p>
                         {getDuree(it.duration)}
-                        <div>Heure d'arrivée : {getHeure(it.arrival_date_time)}</div>
+                        <div className="arrivee">Arrivée {getHeure(it.arrival_date_time)}</div>
                     </div>);
             case "public_transport":
                 return(
                     <div key={index} className="transport">
-                        <p>Transport</p>
+                        <h1>Transport</h1>
                         {getDuree(it.duration)}
-                        <p>CO2 : {it.co2_emission.value} {it.co2_emission.unit}</p>
+                        <p>CO2 : {Math.floor(it.co2_emission.value)} {it.co2_emission.unit} CO2</p>
                         <div>
                             <p className="ligne">{it.display_informations.code}</p>
                             <p>Direction : {it.display_informations.direction}</p>
-                            <div>Heure d'arrivée : {getHeure(it.arrival_date_time)}</div>
+                            <div className="arrivee">Arrivée {getHeure(it.arrival_date_time)}</div>
                         </div>
                     </div>);
-            case "transfert":
+            case "transfer":
                 return (
-                    <div>
-                        Transfert : Marche / Attente
+                    <div key={index} className="marche">
+                        <h1>Transfert</h1>
+                        {getDuree(it.duration)}
+                        <p>{it.to.name}</p>
+                    </div>
+                );
+            case "waiting":
+                return(
+                    <div key={index} className="marche">
+                        <h1>Attendre</h1>
+                        {getDuree(it.duration)}
                     </div>
                 );
             default:
@@ -133,44 +142,66 @@ const Search = () => {
     }
 
     useEffect(()=>{
+        console.log("bonjour")
         getItineraire();
     }, [coordonnees])
 
     const changeCoordonnees = e => {
         e.preventDefault();
+        console.log(document.getElementById("lat").value);
+        console.log(document.getElementById("long").value);
         const newCoord = {
-        lat :(document.getElementById("lat").value),
-        long : (document.getElementById("long").value)};
-        setCoordonnees(newCoord);
+            lat :document.getElementById("lat").value,
+            long : document.getElementById("long").value
+        };
+        setCoordonnees({...newCoord});
     }
 
     return (
       <div>
           <form onSubmit={changeCoordonnees}>
-              <input id="lat" type="text" placeholder="latitude"/>
-              <input id="long" type="text" placeholder="longitude"/>
+              <input id="lat" type="text" placeholder="Latitude"/>
+              <input id="long" type="text" placeholder="Longitude"/>
               <button type="submit">Chercher un itinéraire</button>
           </form>
 
-              {perturbation ?
-                  <div className="perturbation">
-                      {perturbation.map( (pert,index) => {
-                          return(
-                              <div>
-                                Status : {pert.status==="active" ? "OK" : pert.status}
-                              </div>
-                          );
-                      })}
+              {perturbation.length ?
+                  <div>
+                      <h2>Perturbation</h2>
+                      <div className="perturbation">
+                          {perturbation.map( (pert,index) => {
+                              return(
+                                  <div className="flex" key={index}>
+                                      <div style={{color : pert.severity.color}}>
+                                        {pert.status.toUpperCase()}
+                                      </div>
+                                        {pert.cause.toLowerCase()}
+                                  </div>
+                              );
+                          })}
+                      </div>
                   </div>
                   :
                   null
               }
-              {itineraire ?
-                  itineraire.map( (it,index) => renduIt(index,it))
+              {
+              journeys ?
+                  <div className="">
+                      {journeys.map((j, index) => {
+                          const it = j.sections;
+                          console.log(it);
+                          return (
+                              <div className="trajet" key={index}>
+                                  <h2>Trajet {index + 1}</h2>
+                                  {it.map((it, index) => renduIt(index, it))}
+                              </div>
+                          );
+                      })
+                      }
+                  </div>
                   :
                   null
               }
-
       </div>
     );
 }
