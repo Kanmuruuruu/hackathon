3329d547328fbcd42af2614f4546fb271c4899b1
Gautier Gueri <gautiergueri@gmail.com>
2019-12-19 21:17:26 +0100

    Fonctionnel git add .!

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,59 +2,60 @@ import React, {useState, useEffect} from 'react';
 import './Search.css';
 
 
-function returnString(readable){  // Return an array with the string to make a perfectly readable sentence.
-    let string="";
+function returnString(readable) {  // Return an array with the string to make a perfectly readable sentence.
+    let string = "";
     for (let [key, value] of Object.entries(readable)) {
-        string+=',';
-        if(value===0){}
-        else
-        if(value>1){
-            string+=value+" "+key+"s";
-        }
-        else{
-            string+=value+" "+key;
+        string += ',';
+        if (value === 0) {
+        } else if (value > 1) {
+            string += value + " " + key + "s";
+        } else {
+            string += value + " " + key;
         }
     }
-    return string.split(",").filter( val => { return val !== ''});
+    return string.split(",").filter(val => {
+        return val !== ''
+    });
 }
 
 
 //Because I like complicate my life :D
 
-function formatDuration (reste) {
-    if(reste===0){return "now";}
+function formatDuration(reste) {
+    if (reste === 0) {
+        return "now";
+    }
     const readable = {
-        year:0,
-        day:0,
-        hour:0,
-        min:0,
-        sec:0,
+        year: 0,
+        day: 0,
+        hour: 0,
+        min: 0,
+        sec: 0,
     }
-    while(reste>=31536000){
+    while (reste >= 31536000) {
         readable.year++;
-        reste-=31536000;
+        reste -= 31536000;
     }
-    while(reste>=86400){
+    while (reste >= 86400) {
         readable.day++;
-        reste-=86400;
+        reste -= 86400;
     }
-    while(reste>=3600){
+    while (reste >= 3600) {
         readable.hour++;
-        reste-=3600;
+        reste -= 3600;
     }
-    readable.min=Math.floor(reste/60);
-    readable.sec=reste%60;
+    readable.min = Math.floor(reste / 60);
+    readable.sec = reste % 60;
     const aux = returnString(readable);
     //Make sure to add "," and "and" whe it's necessary.
-    let res="";
-    for(let i=0; i <aux.length; i++){
-        if(i===aux.length-1 && i!==0){
-            res+=" et "+aux[i];
-        }
-        else{
-            res+=aux[i];
-            if(i<aux.length-2){
-                res+=", ";
+    let res = "";
+    for (let i = 0; i < aux.length; i++) {
+        if (i === aux.length - 1 && i !== 0) {
+            res += " et " + aux[i];
+        } else {
+            res += aux[i];
+            if (i < aux.length - 2) {
+                res += ", ";
             }
         }
     }
@@ -63,12 +64,12 @@ function formatDuration (reste) {
 
 const Search = () => {
 
-    const [coordonnees,setCoordonnees] = useState({
+    const [coordonnees, setCoordonnees] = useState({
         lat: "", // 1.90392
-        long:"" // 47.90243
+        lng: "" // 47.90243
     });
 
-    const [perturbation, setPerturbation] =useState([]);
+    const [perturbation, setPerturbation] = useState([]);
     const [journeys, setJourneys] = useState([]);
 
 
@@ -78,7 +79,7 @@ const Search = () => {
 
     const getHeure = h => {
         const heure = h.split('T')[1];
-        return (<p>: {heure[0]+heure[1]}:{heure[2]+heure[3]}</p>)
+        return (<p>: {heure[0] + heure[1]}:{heure[2] + heure[3]}</p>)
     }
 
     /*
@@ -91,15 +92,15 @@ const Search = () => {
     */
 
     const getItineraire = async () => {
-        const lat=coordonnees.lat;
-        const long=coordonnees.long;
-        const response = await fetch("https://api.navitia.io/v1/coverage/fr-cen/journeys?from=1.89473%3B47.89386&to="+lat+"%3B"+long+"&",{headers: {Authorization: 'a3653e1d-06a1-4edc-b768-c9bd561d3251'}});
+        const lat = coordonnees.lng;
+        const lng = coordonnees.lat;
+        const response = await fetch("https://api.navitia.io/v1/coverage/fr-cen/journeys?from=1.89473%3B47.89386&to=" + lat + "%3B" + lng + "&", {headers: {Authorization: 'a3653e1d-06a1-4edc-b768-c9bd561d3251'}});
         const data = await response.json();
         setPerturbation(data.disruptions);
         setJourneys(data.journeys);
     }
 
-    const renduIt = (index,it) => {
+    const renduIt = (index, it) => {
         switch (it.type) {
             case "street_network":
                 return (
@@ -110,7 +111,7 @@ const Search = () => {
                         <div className="arrivee">Arrivée {getHeure(it.arrival_date_time)}</div>
                     </div>);
             case "public_transport":
-                return(
+                return (
                     <div key={index} className="transport">
                         <h1>Transport</h1>
                         {getDuree(it.duration)}
@@ -130,7 +131,7 @@ const Search = () => {
                     </div>
                 );
             case "waiting":
-                return(
+                return (
                     <div key={index} className="marche">
                         <h1>Attendre</h1>
                         {getDuree(it.duration)}
@@ -141,68 +142,96 @@ const Search = () => {
         }
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         console.log("bonjour")
         getItineraire();
     }, [coordonnees])
 
-    const changeCoordonnees = e => {
+    const changeCoordonnees = async e => {
         e.preventDefault();
-        console.log(document.getElementById("lat").value);
-        console.log(document.getElementById("long").value);
-        const newCoord = {
-            lat :document.getElementById("lat").value,
-            long : document.getElementById("long").value
-        };
+        const adres = (document.getElementById("adresse").value);
+        adres.replace(" ","%");
+        const response= await fetch("https://api.opencagedata.com/geocode/v1/json?q="+adres+"&key=e48ab858fd614c98a063d2ba25df67ca&language=fr&pretty=1");
+        const data = await response.json();
+        console.log(data.results[0]);
+        let newCoord = data.results[0].geometry;
+        console.log(newCoord);
         setCoordonnees({...newCoord});
     }
 
     return (
-      <div>
-          <form onSubmit={changeCoordonnees}>
-              <input id="lat" type="text" placeholder="Latitude"/>
-              <input id="long" type="text" placeholder="Longitude"/>
-              <button type="submit">Chercher un itinéraire</button>
-          </form>
+        <div>
+            <div className="header">
+                <a href="http://www.google.com">
+                    <img src={require("../image/streamline-icon-lock-unlock-1@40x40.png")} alt=""/>
+                </a>
+                <a href="http://www.google.com">
+                    <img src={require("../image/developmentdurable.png")} alt=""/>
+                </a>
+                <a href="http://www.google.com">
+                    <img src={require("../image/Etoile.png")} alt=""/>
+                </a>
+            </div>
+            <form onSubmit={changeCoordonnees}>
+                <input id="adresse" type="text" placeholder="Destination"/>
+                <button type="submit">Chercher un itinéraire</button>
+            </form>
 
-              {perturbation.length ?
-                  <div>
-                      <h2>Perturbation</h2>
-                      <div className="perturbation">
-                          {perturbation.map( (pert,index) => {
-                              return(
-                                  <div className="flex" key={index}>
-                                      <div style={{color : pert.severity.color}}>
+            {perturbation.length ?
+                <div className="perturbation">
+                    <h2>Perturbation</h2>
+                    <div>
+                        {perturbation.map((pert, index) => {
+                            return (
+                                <div className="flex" key={index}>
+                                    <div style={{color: pert.severity.color}}>
                                         {pert.status.toUpperCase()}
-                                      </div>
-                                        {pert.cause.toLowerCase()}
-                                  </div>
-                              );
-                          })}
-                      </div>
-                  </div>
-                  :
-                  null
-              }
-              {
-              journeys ?
-                  <div className="">
-                      {journeys.map((j, index) => {
-                          const it = j.sections;
-                          console.log(it);
-                          return (
-                              <div className="trajet" key={index}>
-                                  <h2>Trajet {index + 1}</h2>
-                                  {it.map((it, index) => renduIt(index, it))}
-                              </div>
-                          );
-                      })
-                      }
-                  </div>
-                  :
-                  null
-              }
-      </div>
+                                    </div>
+                                    {pert.cause.toLowerCase()}
+                                </div>
+                            );
+                        })}
+                    </div>
+                </div>
+                :
+                null
+            }
+            {
+                journeys ?
+                    <div className="">
+                        {journeys.map((j, index) => {
+                            const it = j.sections;
+                            console.log(it);
+                            return (
+                                <div className="trajet" key={index}>
+                                    <h2>Trajet {index + 1}</h2>
+                                    {it.map((it, index) => renduIt(index, it))}
+                                </div>
+                            );
+                        })
+                        }
+                    </div>
+                    :
+                    null
+            }
+            <div className="widget">
+                <a href="http://www.google.com">
+                    <img src={require("../image/streamline-icon-biking-person@40x40.png")} alt=""/>
+                </a>
+                <a href="http://www.google.com">
+                    <img src={require("../image/streamline-icon-truck-2@40x40.png")} alt=""/>
+                </a>
+                <a href="http://www.google.com">
+                    <img src={require("../image/streamline-icon-cable-car-1@40x40.png")} alt=""/>
+                </a>
+                <a href="http://www.google.com">
+                    <img src={require("../image/streamline-icon-parking-p-1@40x40.png")} alt=""/>
+                </a>
+                <a href="http://www.google.com">
+                    <img src={require("../image/streamline-icon-transportation-ticket-train@40x40.png")} alt=""/>
+                </a>
+            </div>
+        </div>
     );
 }
 
