b674cdb76bed4fdebe5747c934ad4b29ac921565
Gautier Gueri <gautiergueri@gmail.com>
2019-12-20 09:18:47 +0100

    Ajout de la navigation

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,20 +1,31 @@
-import React from 'react'
+import React from 'react';
+import {Switch, Route, NavLink} from 'react-router-dom';
+import Fav from './Fav';
+import Home from './Home';
+import Mobilit from './Mobilit';
+import CompteClient from "./CompteClient"
 
 const NavBar = () => {
     return (
         <div className="header">
-            <a href="http://www.google.com">
+            <NavLink to="/">
                 <img src={require("../image/streamline-icon-house-1@40x40.png")} alt=""/>
-            </a>
-            <a href="http://www.google.com">
+            </NavLink>
+            <NavLink to="/client">
                 <img src={require("../image/streamline-icon-lock-unlock-1@40x40.png")} alt=""/>
-            </a>
-            <a href="http://www.google.com">
-                <img src={require("../image/developmentdurable.png")} alt=""/>
-            </a>
-            <a href="http://www.google.com">
+            </NavLink>
+            <NavLink to="/mobilite">
+                <img src={require("../image/streamline-icon-gmo-food-leaf-dna@40x40.png")} alt=""/>
+            </NavLink>
+            <NavLink to="/favoris">
                 <img src={require("../image/etoileNoir.png")} alt=""/>
-            </a>
+            </NavLink>
+        <Switch>
+            <Route exact path="/" components={Home}/>
+            <Route path="/favoris" components={Fav} />
+            <Route path="/mobilite" components={Mobilit}/>
+            <Route path="/client" components={CompteClient}/>
+        </Switch>
         </div>
     );
 }
